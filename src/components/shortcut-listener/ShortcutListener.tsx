71cb4a72d73b6dd5fd651ed1fa1175cc7ecc0587
Jeroen Weener <jweener1996@gmail.com>
2022-07-15 00:48:20 +0200

    :bug: Shortcut keys are unregistered after leaving project page

--- src/components/shortcut-listener/ShortcutListener.tsx
+++ src/components/shortcut-listener/ShortcutListener.tsx
@@ -4,6 +4,8 @@
  * Pure logic component listening to keyboard events.
  */
 
+import { useEffect } from "react"
+
 interface ShortcutListenerProps {
     onMute: () => void
     onToggle: () => void
@@ -15,70 +17,74 @@ const ShortcutListener = ({
     onToggle,
     onSeekRelative,
 }: ShortcutListenerProps) => {
-    document.body.onkeydown = (e) => {
-        if (
-            e.key === ' ' ||
-            e.code === 'Space' ||
-            e.keyCode === 32
-        ) {
-            e.preventDefault()
-            onToggle()
-        }
+    useEffect(() => {
+        const handleKeydown = (e: any): void => {
+            if (
+                e.key === ' ' ||
+                e.code === 'Space' ||
+                e.keyCode === 32
+            ) {
+                e.preventDefault()
+                onToggle()
+            }
 
-        if (
-            e.key === 'ArrowLeft' ||
-            e.code === 'ArrowLeft' ||
-            e.keyCode === 37
-        ) {
-            e.preventDefault()
-            onSeekRelative(-5)
-        }
+            if (
+                e.key === 'ArrowLeft' ||
+                e.code === 'ArrowLeft' ||
+                e.keyCode === 37
+            ) {
+                e.preventDefault()
+                onSeekRelative(-5)
+            }
 
-        if (
-            e.key === 'ArrowRight' ||
-            e.code === 'ArrowRight' ||
-            e.keyCode === 39
-        ) {
-            e.preventDefault()
-            onSeekRelative(5)
-        }
+            if (
+                e.key === 'ArrowRight' ||
+                e.code === 'ArrowRight' ||
+                e.keyCode === 39
+            ) {
+                e.preventDefault()
+                onSeekRelative(5)
+            }
 
-        if (
-            e.key === 'j' ||
-            e.code === 'KeyJ' ||
-            e.keyCode === 74
-        ) {
-            e.preventDefault()
-            onSeekRelative(-10)
-        }
+            if (
+                e.key === 'j' ||
+                e.code === 'KeyJ' ||
+                e.keyCode === 74
+            ) {
+                e.preventDefault()
+                onSeekRelative(-10)
+            }
 
-        if (
-            e.key === 'l' ||
-            e.code === 'KeyL' ||
-            e.keyCode === 76
-        ) {
-            e.preventDefault()
-            onSeekRelative(10)
-        }
+            if (
+                e.key === 'l' ||
+                e.code === 'KeyL' ||
+                e.keyCode === 76
+            ) {
+                e.preventDefault()
+                onSeekRelative(10)
+            }
 
-        if (
-            e.key === 'k' ||
-            e.code === 'KeyK' ||
-            e.keyCode === 75
-        ) {
-            e.preventDefault()
-            onToggle()
-        }
+            if (
+                e.key === 'k' ||
+                e.code === 'KeyK' ||
+                e.keyCode === 75
+            ) {
+                e.preventDefault()
+                onToggle()
+            }
 
-        if (
-            e.key === 'm' ||
-            e.code === 'KeyM' ||
-            e.keyCode === 77
-        ) {
-            e.preventDefault()
-            onMute()
+            if (
+                e.key === 'm' ||
+                e.code === 'KeyM' ||
+                e.keyCode === 77
+            ) {
+                e.preventDefault()
+                onMute()
+            }
         }
-    }
+        document.body.addEventListener('keydown', handleKeydown)
+        return () => document.body.removeEventListener('keydown', handleKeydown)
+    }, [onMute, onSeekRelative, onToggle])
 
     return <></>
 }
